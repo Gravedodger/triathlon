3a13fc3e772058fcd6e4c14c80f16f5aec0d4879
Sviatoslav <61473892+Gravedodger@users.noreply.github.com>
2021-03-10 15:46:34 +0200

    side menu links reformatted

--- components/misc/Menus/TemporaryDrawer.js
+++ components/misc/Menus/TemporaryDrawer.js
@@ -8,8 +8,8 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import MenuSharpIcon from '@material-ui/icons/MenuSharp';
 import styles from '../../../styles/Modules/TemporaryDrawer.module.css';
-import ChevronRightSharpIcon from '@material-ui/icons/ChevronRightSharp'
-import Link from 'next'
+import ChevronRightSharpIcon from '@material-ui/icons/ChevronRightSharp';
+import Link from 'next';
 
 const useStyles = makeStyles({
   list: {
@@ -45,84 +45,84 @@ export default function TemporaryDrawer() {
     >
       <List className={styles.list}>
         <ListItem>
-          <Link href='/'><a className={styles.listItem}>
+          <Link href="/"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Головна</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem>
-          <Link href='/About'><a className={styles.listItem}>
+          <Link href="/About"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Про нас</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem>
-          <Link href='/News'><a className={styles.listItem}>
+          <Link href="/News"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Новини</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='/SportsAndRules'><a className={styles.listItem}>
+          <Link href="/SportsAndRules"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Види спорту та правила</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='/TrainingCenters'><a className={styles.listItem}>
+          <Link href="/TrainingCenters"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Тренувальні центри</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='/Athletes'><a className={styles.listItem}>
+          <Link href="/Athletes"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Атлети</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='/Services'><a className={styles.listItem}>
+          <Link href="/Services"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Послуги атлетам</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
         </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='/Safety'><a className={styles.listItem}>
+          <Link href="/Safety"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Безпека атлетів</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='/Donate'><a className={styles.listItem}>
+          <Link href="/Donate"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Пожертви пректу</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='/Sponsors'><a className={styles.listItem}>
+          <Link href="/Sponsors"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Спонсори</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='Partners'><a className={styles.listItem}>
+          <Link href="Partners"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Партнери</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
         </ListItem>
         <Divider />
         <ListItem button>
-          <Link href='PressInfo'><a className={styles.listItem}>
+          <Link href="PressInfo"><a className={styles.listItem}>
             <ListItemText className={styles.listText}>Для Преси</ListItemText>
             <ChevronRightSharpIcon className={styles.icnBlue} />
           </a></Link>
