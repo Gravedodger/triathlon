b7d812847e63d8e6252bea86f89ee07aec44284c
Sviatoslav <61473892+Gravedodger@users.noreply.github.com>
2021-03-10 06:50:38 +0200

    Rebuild commit

--- components/misc/Menus/LanguageSwitch.js
+++ components/misc/Menus/LanguageSwitch.js
@@ -3,6 +3,7 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import {IoLanguageSharp} from 'react-icons/io5';
 import styles from '../../../styles/Modules/Header.module.css';
+import Link from 'next/link';
 
 export default function LanguageSwitch() {
   const [anchorEl, setAnchorEl] = React.useState(null);
